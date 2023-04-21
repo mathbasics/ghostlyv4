683554e08e1c04fec77324d9362204c3efb19746
elyte <129152672+mathbasics@users.noreply.github.com>
2023-04-21 17:24:04 +1000

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,11 @@
+		function saveName() {
+			const name = document.getElementById("name").value;
+			localStorage.setItem("name", name);
+		}
+		
+		function saveIcon() {
+			const icon = document.getElementById("icon").value;
+			localStorage.setItem("icon", icon);
+		}
+  });
+});
